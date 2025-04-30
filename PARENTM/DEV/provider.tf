0a0c93bd5260a17de200f3dce57a8eeb27c16fed
SOMNATH VERMA <somverma.earth@outlook.com>
2025-04-30 03:51:20 -0400

    11thtry

--- PARENTM/DEV/provider.tf
+++ PARENTM/DEV/provider.tf
@@ -9,9 +9,9 @@ terraform {
 backend "azurerm" {
 subscription_id = "2178b722-5baa-4418-8bc9-3fcb9cd14fed"
     resource_group_name = "DevOpsRG"
-    storage_account_name = "devstg2025"
+    storage_account_name = "stg2025n"
     container_name = "devcont"
-    access_key = "imp.tfstate"
+    access_key = "newdevv.tfstate"
   
 }
 
